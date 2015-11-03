df25b9252c7e62c5261b3451096f078202f0537e
daisukeapp <nizumical@gmail.com>
2015-11-03 10:38:07 +0900

    Initial Commit

---
+++ TestChamTests/TestChamTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestChamTests.swift
+//  TestChamTests
+//
+//  Created by 仁泉大輔 on 11/3/15.
+//  Copyright © 2015 仁泉大輔. All rights reserved.
+//
+
+import XCTest
+@testable import TestCham
+
+class TestChamTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
